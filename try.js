70aeff06c7dcfd8d682674853ae3967f9bd823cb
hershelsch <117215184+hershelsch@users.noreply.github.com>
2023-12-11 15:37:02 -0500

    new commit

---
+++ try.js
@@ -0,0 +1,66 @@
+const minyunim = [
+    {cheider:"א",time:"7:00"},
+    {cheider:"א",time:"8:00"},
+    {cheider:"א",time:"9:00"},
+    {cheider:"א",time:"10:00"},
+    {cheider:"א",time:"11:00"},
+    {cheider:"א",time:"12:00"},
+    {cheider:"ב",time:"7:15"},
+    {cheider:"ב",time:"8:15"},
+    {cheider:"ב",time:"9:15"},
+    {cheider:"ב",time:"10:15"},
+    {cheider:"ב",time:"11:15"},
+    {cheider:"ב",time:"12:15"},
+    {cheider:"ג",time:"7:30"},
+    {cheider:"ג",time:"8:30"},
+    {cheider:"ג",time:"9:30"},
+    {cheider:"ג",time:"10:30"},
+    {cheider:"ג",time:"11:30"},
+    {cheider:"ג",time:"12:30"},
+    {cheider:"ד",time:"7:45"},
+    {cheider:"ד",time:"8:45"},
+    {cheider:"ד",time:"9:45"},
+    {cheider:"ד",time:"10:45"},
+    {cheider:"ד",time:"11:45"},
+    {cheider:"ד",time:"12:45"}
+];
+const cheiderim = ["א","ב","ג","ד"];
+const times = ["7:00","7:15","7:30","7:45",
+"8:00","8:15","8:30","8:45","9:00","9:15",
+"9:30","9:45","10:00","10:15","10:30","10:45",
+"11:00","11:15","11:30","11:45","12:00",
+"12:15","12:30","12:45"];
+const days = ["ראשון","שני","שלישי","רביעי","חמישי"];
+const daysNum = [1,2,3,4,5];
+const cheiderimNum = [1,2,3,4];
+const timesNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,
+    15,16,17,18,19,20,21,22,23,24];
+const cheiderimTimes = [
+
+    {cheider:"א",time:"7:00"},
+    {cheider:"א",time:"8:00"},
+    {cheider:"א",time:"9:00"},
+    {cheider:"א",time:"10:00"},
+    {cheider:"א",time:"11:00"},
+    {cheider:"א",time:"12:00"},
+    {cheider:"ב",time:"7:15"},
+    {cheider:"ב",time:"8:15"},
+    {cheider:"ב",time:"9:15"},
+    {cheider:"ב",time:"10:15"},
+    {cheider:"ב",time:"11:15"},
+    {cheider:"ב",time:"12:15"},
+    {cheider:"ג",time:"7:30"},
+
+    {cheider:"ג",time:"8:30"},
+
+    {cheider:"ג",time:"9:30"},
+    {cheider:"ג",time:"10:30"},
+    {cheider:"ג",time:"11:30"},
+    {cheider:"ג",time:"12:30"},
+    {cheider:"ד",time:"7:45"},
+    {cheider:"ד",time:"8:45"},
+    {cheider:"ד",time:"9:45"},
+    {cheider:"ד",time:"10:45"},
+    {cheider:"ד",time:"11:45"},
+    {cheider:"ד",time:"12:45"}
+];
